f345056dcee8293114c056b78c8108d56a60e3c4
wahi90 <mdshahed1098@gmail.com>
2022-12-04 12:12:32 +0600

    done protected route also

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,33 @@
+import {signOut } from "firebase/auth";
 import React from 'react';
+import toast from "react-hot-toast";
 import '../css/Sidebar.css'
+import auth from '../firebase-init'
 
+import {   useNavigate } from 'react-router-dom';
 const Profile = () => {
+    const navigate = useNavigate();
+    
+    const handleSignOut = () => {
+
+        signOut(auth).then(() => {
+           toast.success("Logout Successfully")
+           setTimeout(() => {
+            navigate('/login');
+           }, 1000);
+        }).catch((error) => {
+            toast.error("Logout failed")
+});
+  // An error happened.
+}
+
     return (
+
+        
         <div className='profileContainer'>
             <img  src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
             <h2 className='text-white'>Mohammad Sahed</h2>
-            <button >Logout</button>
+            <button onClick={handleSignOut} >Logout</button>
             
         </div>
     );
--- src/components/Protected.js
+++ src/components/Protected.js
@@ -1,9 +1,12 @@
+import auth from "../firebase-init";
 import { Navigate } from "react-router-dom";
 
 
 const Protected = ({children}) => {
-const user = true;
 
+
+const user = auth.currentUser;
+console.log(auth.currentUser);
 if(!user){
     return <Navigate to={'/login'} replace></Navigate>
 }
